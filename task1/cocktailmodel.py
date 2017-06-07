8d7774a18d374620ca4d543f91a5aa62fcd4fd0c
dslize <d.schleiz@web.de>
2017-06-07 17:19:13 +0200

    finish task1

--- task1/cocktailmodel.py
+++ task1/cocktailmodel.py
@@ -5,13 +5,16 @@ from gurobipy import *
 def solve(zutaten, preis, alkohol, zucker, kalorien, minAlkohol, maxAlkohol, minZucker, maxKalorien, minVolumen):
   # Modell erzeugen:
   model = Model("Cocktailmischung")
+  
+  #Minimierungsproblem
+  model.modelSense = GRB.MINIMIZE
 
   # Variablen erzeugen: Zunaechst leeres Dictionary.
   x = {} 
 
   # Alle Variablen der Liste durchlaufen.
   for zutat in zutaten:
-    # Fuer jede Zutat wird die Variable "x_ZUTAT" angelegt.
+    # Fuer jede Zutat wird die Variable "x_ZUTAT" angelegt. Implizit lower bound = 0.0.
     x[zutat] = model.addVar(name = ("x_" + zutat), obj = preis[zutat]) 
 
   # Variablen dem Modell bekannt machen.
@@ -20,28 +23,29 @@ def solve(zutaten, preis, alkohol, zucker, kalorien, minAlkohol, maxAlkohol, min
   # Erste Nebenbedingung:
   # Bedeutung: Volumen muss mindestens <minVolumen> sein.
   # Ungleichung: "Summe aller Variablen >= minVolumen"
-
-  model.addConstr( "HIER FEHLT DIE UNGLEICHUNG!" )
+  model.addConstr(quicksum(x[zutat] for zutat in zutaten) >= minVolumen)
 
   # Zweite Nebenbedingung:
-  # Bedeutung: ...
-  # Ungleichung: ...
-
+  # Bedeutung: Alkoholgehalt muss mindestens <minAlkohol> sein.
+  # Ungleichung: "Summe der Alkoholgehalte multipliziert mit der Anzahl der Zutat, geteilt durch Summe aller Variablen >= minAlkohol"
+  #              "Obere Bedingung nicht linear, also auf beiden Seiten mit Nenner multiplizieren, legitim da Variablen nichtnegativ"
+  model.addConstr(quicksum(alkohol[zutat] * x[zutat] for zutat in zutaten) >= minAlkohol * quicksum(x[zutat] for zutat in zutaten))
 
   # Dritte Nebenbedingung:
-  # Bedeutung: ...
-  # Ungleichung: ...
-
+  # Bedeutung: Alkoholgehalt muss höchstens <maxAlkohol> sein.
+  # Ungleichung: "Summe der Alkoholgehalte multipliziert mit der Anzahl der Zutat, geteilt durch Summe aller Variablen <= minAlkohol"
+  #              "Obere Bedingung nicht linear, also auf beiden Seiten mit Nenner multiplizieren, legitim da Variablen nichtnegativ"
+  model.addConstr(quicksum(alkohol[zutat] * x[zutat] for zutat in zutaten) <= maxAlkohol * quicksum(x[zutat] for zutat in zutaten))
 
   # Vierte Nebenbedingung:
-  # Bedeutung: ...
-  # Ungleichung: ...
-
+  # Bedeutung: Zuckermenge muss mindestens <minZucker> sein.
+  # Ungleichung: "Summe aller Zuckermengen, jeweils multipliziert mit der Anzahl der Zutat >= minZucker"
+  model.addConstr(quicksum(zucker[zutat] * x[zutat] for zutat in zutaten) >= minZucker)
 
   # Fuenfte Nebenbedingung:
-  # Bedeutung: ...
-  # Ungleichung: ...
-
+  # Bedeutung: Kalorienanzahl muss höchstens <maxKalorien> sein.
+  # Ungleichung: "Summe aller Kalorienanzahlen, jeweils multipliziert mit der Anzahl der Zutat <= maxKalorien"
+  model.addConstr(quicksum(kalorien[zutat] * x[zutat] for zutat in zutaten) <= maxKalorien)
 
   # Nebenbedingungen hinzugefuegt? LP loesen lassen!
   model.optimize()
